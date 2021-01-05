c4623dd13ad4e5d579f983cf2228a3f7e66c0c80
sana mahamid <sanaa_6-5.7@hotmail.com>
2021-01-05 21:29:31 +0200

    done

--- src/exercises/Exercise2.js
+++ src/exercises/Exercise2.js
@@ -1,14 +1,32 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
 
 export default function Exercise2() {
-  // your code here
+  const [users, setUsers] = useState([])
+
+  useEffect(() => {
+    (async () => {
+      const users = await axios.get('https://randomuser.me/api/?results=5')
+      setUsers(users.data.results)
+    })()
+
+
+  }, [])
 
 
   return (
     <div className="ex-space">
       <h4 className="ex-title">Exercise 2</h4>
       <div className="exercise" id="ex-2">
-        {/* your code here... */}
+        {users.map((u,i) => {
+          return (
+            <div key={i}>
+              <span>{u.name.first} </span>
+              <span>{u.name.last} </span>
+              <img src={u.picture.thumbnail} alt="" />
+            </div>
+          )
+        })}
 
       </div>
     </div>
--- src/spot-checks/SpotCheck1.js
+++ src/spot-checks/SpotCheck1.js
@@ -1,14 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 export default function SpotCheck1() {
   // your code here
-  
+  const [counter, setCounter] = useState(0)
+  const increase = () => setCounter(counter+1)
+  const decrease = () => setCounter(counter-1)
 
   return (
     <div className="ex-space">
       <h4 className="ex-title">Spot Check 1</h4>
       <div className="exercise" id="sc-1">
-        {/* your code here... */}
+        <div>Counter: {counter}</div>
+        <button onClick={increase}>Increase</button>
+        <button onClick={decrease}>Decrease</button>
 
       </div>
     </div>
